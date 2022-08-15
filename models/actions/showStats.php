961bb79a5efd5459757980ca7dc83398dcede1b2
kapi1994 <nemanjajovicic13@gmail.com>
2022-08-15 23:50:44 +0200

    showing stats

--- models/actions/showStats.php
+++ models/actions/showStats.php
@@ -1,23 +1,65 @@
 <?php
-header("Content-type:applicaiton/json");
-require_once '../../config/connection.php';
-require '../functions.php';
+header("Content-type:application/json");
 
-$file =  fopen("../../data/logs.txt", 'r');
-$fileData = file('../../data/logs.txt');
-fclose($file);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    require_once '../../config/connection.php';
+    require '../functions.php';
 
-$stats = [];
-$pages = ['home' => 0, 'games' => 0, 'contact' => 0, 'author' => 0];
-$pages24 = ['home' => 0, 'games' => 0, 'contact' => 0, 'author' => 0];
+    $fileData  = fopen('../../data/logs.txt', 'r');
+    $rows = file('../../data/logs.txt');
+    fclose($fileData);
 
-$total = 0;
-$total24 = 0;
+    $stats = [];
 
-// echo json_encode($fileData);
+    $pages = ['home' => 0, 'games' => 0, 'contact' => 0, 'author' => 0];
+    $pages24 = ['home' => 0, 'games' => 0, 'contact' => 0, 'author' => 0];
+
+    $total = 0;
+    $total24 = 0;
 
 
-echo json_encode([
-    'totalOrders' => totalOrders(),
-    'registretedUsers' => totalRegistetedUsers()
-]);
+    foreach ($rows as $row) {
+        $data = explode("\t", $row);
+        $url = $data[0];
+        $url = explode('?', $url);
+        $mainUrl = $url[0];
+        $visited = explode("/", $mainUrl);
+        $index = $visited[count($visited) - 1];
+
+
+        if ($index != 'index.php') {
+            continue;
+        }
+
+        if (count($url) == 1) {
+            $pages['home']++;
+            $total++;
+        } else {
+            $page = $url[1];
+            $page = explode("&", $page)[0];
+            $page = explode("=", $page);
+            if ($page[0] == "page" && count($page) > 1) {
+                if (array_key_exists($page[1], $pages)) {
+                    $pages[$page[1]]++;
+                    $total++;
+
+                    if (inTheLast24H($data[1])) $pages24[$page[1]]++;
+                }
+            }
+        }
+    }
+
+    $logins = loggedInToday();
+    $registeredUsers = totalRegistetedUsers();
+    $totalOrders  = totalOrders();
+
+    echo json_encode([
+        'overallViews' => $pages,
+        'todayViews' => $pages24,
+        'todayLogins' => $logins,
+        'registeredUsers' => $registeredUsers,
+        'totalOrders' => $totalOrders
+    ]);
+} else {
+    http_response_code(404);
+}
