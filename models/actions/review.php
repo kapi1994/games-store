33465a96c50651541d28505405f851104e910590
kapi1994 <nemanjajovicic13@gmail.com>
2022-08-17 00:38:33 +0200

    Functionality to add user reviews

--- models/actions/review.php
+++ models/actions/review.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 header("Content-type:application/json");
-if ($_SERVER["REQEUST_METHOD"] == "POST") {
-    $game_edition_id = $_POST['id'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $game_edition_id = $_POST['game_edition_id'];
     $rating = $_POST['rating'];
     $message = $_POST['message'];
     $user_id  = isset($_SESSION['user']) ? $_SESSION['user']->id : '';
-
+    $errors = [];
     if ($rating == 'default') {
         $errors[] = "Please choose rating";
     }
@@ -25,7 +25,8 @@ if ($_SERVER["REQEUST_METHOD"] == "POST") {
             require '../functions.php';
 
             $lastInsertReview = insertReview($rating, $message, $game_edition_id, $user_id);
-            $lastReview = getLastReview($lastInsertReview);
+            $lastReview = getLastReview($lastInsertReview, $user_id);
+            echo json_encode($lastReview);
         } catch (PDOException $th) {
             echo json_encode($th->getMessage());
             http_response_code(500);
